2ba6632acc91524aaa811d273f24b26e3b0ba6bf
spitfjre <spitfjre@gmx.de>
2021-05-02 20:19:45 +0200

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wmg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
